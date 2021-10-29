7d2dc7485482d4fc8aa99e74214cdf878c51aecb
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-10-29 15:32:35 -0300

    deconstructed req.body  and removed showMessage

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,6 @@
-export const showMessage = (req,res)=>{
-    res.status(200).send(`Here is your message: ${req.params.message}`);
+
+import User from '../models/user'
+export const register = async (req, res) => {
+console.log(req.body)
+const {name,email,password} = req.body
 }
