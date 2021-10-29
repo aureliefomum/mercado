ce1f85673476daa23611d9bda6aad9d865c35047
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-10-29 15:17:33 -0300

    created user model in models folder

---
+++ server/models/user.js
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt'
+const {Schema} = mongoose
+
+
+const userSchema = new Schema ({
+    name: {
+        type:String,
+        trim:true,
+        required: 'Name is required'
+    },
+
+    email: {
+        type:String,
+        trim:true,
+        required: 'Email is required',
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+        max:64
+    },
+     stripe_account_id: '',
+     stripe_seller: {},
+     stripeSession: {}
+    
+}, {timestamps: true})
+
+
+//use 'pre' middleware, it should use a regular function, not an arrow function
+
+userSchema.pre('save', function (next){
+    let user = this;
+
+    // check if user was modified, if yes hash their password
+    if(user.isModified('password')){
+        return bcrypt.hash(user.password, 12, function (err,hash){
+            if(err){
+                console.log('BCRYPT HASH', err);
+                return next(err);
+            }
+            user.password = hash;
+            return next()
+        });
+    } else{
+        return next();
+    }
+})
+
+
+
+
+
+export default mongoose.model("User", userSchema)
