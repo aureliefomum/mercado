6ac632526c2075b4e758f41d5004e63da4ba912e
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-10-30 13:00:40 -0300

    added express.json middleware

--- server/server.js
+++ server/server.js
@@ -1,7 +1,27 @@
 import express from "express";
 import {readdirSync} from 'fs';
+import cors from 'cors';
+import mongoose from 'mongoose'
+const morgan = require('morgan');
+require('dotenv').config();
+
 
 const app = express();
+
+// db connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    
+})
+.then(()=> console.log('Database connected successfully!'))
+.catch((err)=> console.log('DB Connection Error: ', err))
+
+
+//middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
 
 //route middleware
 readdirSync('./routes').map((r) => 
@@ -9,6 +29,6 @@ app.use('/api', require(`./routes/${r}`))
 );
 
 
+const port= process.env.PORT
 
-
-app.listen(8000, ()=> console.log(`Server is running on port 8000`))
+app.listen(port, ()=> console.log(`Server is running on port ${port}`))
