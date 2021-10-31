781f5f38360c796e9d916dc7a9be617c54583eaf
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-10-31 14:05:28 -0300

    imported ToastContainer

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Home from './booking/Home';
 import Login from './auth/Login';
 import Register from './auth/Register';
 import TopNav from './components/TopNav';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 //  create TopNavigation/menu bar to navigate between pages
 //  write it first here in App.js then move to its own component
@@ -20,6 +22,7 @@ function App() {
   return (
     <BrowserRouter>
     <TopNav />
+    <ToastContainer position="top-center"/>
     <Switch>
       <Route exact path="/"  component={Home}/>
       <Route exact path="/login"  component={Login}/>
