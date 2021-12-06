26474b05bdf5a3f3b4ebf8c79fc7aa119c1b6fc7
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-12-06 15:26:30 -0300

    added logout functionality to TopNav

--- client/src/components/TopNav.js
+++ client/src/components/TopNav.js
@@ -1,18 +1,49 @@
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
 
 
-const TopNav = () => (
-    <div className="nav bg-ight d-flex justify-content-between">
+const TopNav = () => {
+  const dispatch = useDispatch();
+  const { auth } = useSelector((state) =>({ ...state }));
+  const history = useHistory();
+
+  const logout = () => {
+      dispatch({
+        type:"LOGOUT",
+        payload: null,
+      });
+
+      window.localStorage.removeItem("auth")
+
+  }
+
+  return(
+    <div className="nav bg-light d-flex justify-content-between">
       <Link className="nav-link" to="/">
-        Home
+        Home 
         </Link>
-      <Link className="nav-link" to="/login">
+      
+      {auth !== null && (
+        <a className="nav-link pointer" onClick={logout}>
+        Logout 
+        </a>
+      )}
+
+      {auth === null && (
+        <>
+        <Link className="nav-link" to="/login">
         Login
         </Link>
       <Link className="nav-link" to="/register">
         Register
         </Link>
+        </>
+      )}
     </div>
- )
+ );
+};
+   
+ 
 
  export default TopNav
