6b0aea7170c764a1d3fcfab8e463d25a77121819
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-12-07 18:13:14 -0300

    created Dashboard component

---
+++ client/src/user/Dashboard.js
@@ -0,0 +1,27 @@
+import DashBoardNav from "../components/DashBoardNav";
+import ConnectNav from "../components/ConnectNav";
+
+const Dashboard = () => {
+    return (
+        <>
+
+        <div className="container-fluid bg-secondary p-5">
+            <ConnectNav />
+        </div>
+
+        <div className="container-fluid p-4">
+            <DashBoardNav />
+        </div>
+
+
+        <div className="container">
+            <p>Show all bookings and a button to browse hotels</p>
+        </div>
+
+
+
+        </>
+    );
+}
+
+export default Dashboard;
