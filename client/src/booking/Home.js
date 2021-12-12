143986e83b5a7f7ac2015c13b78743f877a85872
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-12-12 07:04:37 -0300

    deleted state from HomePage component

--- client/src/booking/Home.js
+++ client/src/booking/Home.js
@@ -1,14 +1,9 @@
-// bring in a hook called useSelector that allows to access state
-import {userSelector, useSelector, useStore} from 'react-redux'
-
-
-
 const Home = () => {
-        const {user} = useSelector((state)=> ({...state}));
+        
     return (
         
         <div className= "container-fluid h1 p-5 text-center">
-             Home Page {JSON.stringify(user)}
+             Home Page
         </div>
     )
 }
