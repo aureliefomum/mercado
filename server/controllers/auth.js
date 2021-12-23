e4f7d477e788ab35808a184c4c28d96d75cfa735
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-12-23 14:16:51 -0300

    imported json web token

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 
-import User from '../models/user'
+import User from '../models/user';
+import jwt from 'jsonwebtoken';
 
 
 export const register = async (req, res) => {
@@ -32,5 +33,32 @@ try{
 
 
  export const login = async (req, res) => {
-     console.log(req.body)
+    //   console.log(req.body)
+      const {email, password} = req.body
+    try {
+        //check if logging in user exists
+        let user = await User.findOne({ email }).exec();
+        // console.log('USER EXISTS', user)
+        if(!user) res.status(400).send('User with that email not found');
+        //compare password, make sure we're getting the right password
+        user.comparePassword(password, (err,match) => {
+            console.log('COMPARE PASSWORD IN LOGIN ERR', err)
+            if(!match || err) return res.status(400).send('Wrong password');
+            // GENERATE A TOKEN THEN SEND AS RESPONSE TO CLIENT
+            let token = jwt.sign({_id:user._id}, process.env.JWT_SECRET, {
+                expiresIn:'7d'
+            });
+            res.json({token, user : {
+                _id:user._id,
+                name:user.name,
+                email:user.email,
+                createdAt:user.createdAt,
+                updatedAt: user.updatedAt,
+            },
+        })
+        })
+    } catch(err){
+        console.log('LOGIN ERROR', err)
+        res.status(400).send("Signin failed")
+    }
  }
