a45c6dbdf777d58b76c0769dc1196cba6977984a
aureliefomum <azam_aurelie@yahoo.com>
2022-01-07 12:57:28 -0300

    added homeOutlined icon and redirect button

--- client/src/user/DashboardSeller.js
+++ client/src/user/DashboardSeller.js
@@ -1,37 +1,66 @@
 import DashBoardNav from "../components/DashBoardNav";
 import ConnectNav from "../components/ConnectNav";
-import {Link} from 'react-router-dom';
-
-
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { HomeOutlined } from "@ant-design/icons";
 
 const DashboardSeller = () => {
-    return (
-        <>
+	const { auth } = useSelector((state) => ({ ...state }));
 
-        <div className="container-fluid bg-secondary p-5">
-             <ConnectNav />
-        </div>
+	const connected = () => (
+		<div className="container-fluid">
+			<div className="row">
+				<div className="col-md-10">
+					<h2>Your Hotels</h2>
+				</div>
+				<div className="col-md-2">
+					<Link to="/hotels/new" className="btn btn-primary">
+						+ Add New
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 
-        <div className="container-fluid p-4">
-            <DashBoardNav />
-        </div>
+	const notConnected = () => (
+		<div className="container-fluid">
+			<div className="row">
+				<div className="col-md-6 offset-md-3 text-center">
+					<HomeOutlined className="h1" />
+					<h2>Setup payments to post hotel rooms </h2>
+					<p className="lead">
+						MERN partners with Stripe to transfer earnings to your bank account
+					</p>
+					<button className="btn btn-primary mb-3">Setup Payouts</button>
+					<p className="text-muted">
+						<small>
+							You'll be redirected to Stripe to complete the onboarding process.
+						</small>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+	return (
+		<>
+			<div className="container-fluid bg-secondary p-5">
+				<ConnectNav />
+			</div>
 
+			<div className="container-fluid p-4">
+				<DashBoardNav />
+			</div>
 
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-md-10">
-                <h2>Your Hotels</h2> 
-                </div>
-                <div className="col-md-2">
-                  <Link to="/hotels/new" className="btn btn-primary">+ Add New</Link>
-                </div>
-            </div>
+			{auth &&
+			auth.user &&
+			auth.user.stripe_seller &&
+			auth.user.stripe_seller.charges_enabled
+				? connected()
+				: notConnected()}
 
-        </div>
-
-
-        </>
-    );
-}
+			{/* <pre>{JSON.stringify(auth, null, 4)}</pre> */}
+		</>
+	);
+};
 
 export default DashboardSeller;
