a4ba3f8248d765a362d76110b5b16e43e35200e5
aureliefomum <azam_aurelie@yahoo.com>
2022-02-04 16:20:29 -0300

    added stripe create account functionality

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -1,6 +1,33 @@
-import { requireSignin } from "../middlewares";
+import User from "../models/user";
+import Stripe from "stripe";
+
+const stripe = Stripe(process.env.STRIPE_SECRET);
 
 export const createConnectAccount = async (req, res) => {
-	console.log("REQUEST USER FROM REQUIRE SIGNIN", req.user);
-	console.log("YOu HIT CREATE ACCOUNT ENDPOINT");
+	//find user from database
+	const user = await User.findById(req.user._id).exec();
+	console.log("USER===>", user);
+
+	// //if user doesnt have a stripe account ID yet, create one now
+	// const account = await stripe.accounts.create({
+	// 	type: "express",
+	// });
+	// console.log("ACCOUNT ====>", account);
+	if (!user.stripe_account_id) {
+		const account = await stripe.accounts.create({
+			country: "BR",
+			type: "express",
+			capabilities: {
+				card_payments: {
+					requested: true,
+				},
+				transfers: {
+					requested: true,
+				},
+			},
+		});
+		console.log("ACCOUNT => ", account.id);
+		user.stripe_account_id = account.id;
+		await user.save();
+	}
 };
